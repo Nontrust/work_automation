6dccf3d5cb1e028503bdc9dae536930c89bd6ab8
Nontrust <deed1515@naver.com>
2023-10-13 15:03:45 +0900

    :wrench: [config] Spring security 기초 로그인 폼 설정  - default customizer설정

---
+++ src/main/java/com/kakaotalk/work_automation/global/security/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.kakaotalk.work_automation.global.security.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+}
